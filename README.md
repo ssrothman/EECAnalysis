dfc3d23356d3ea5b8210795b9202a0687762a409
Simon Rothman <srothman@mit.edu>
2022-04-28 15:28:25 -0500

    working environment at long last; started framework

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EECAnalysis
 
-Setup: need env with uproot, awkward, etc
-
-EEC library needs pyfjcore 
+need python=3.9
+conda install from envlist
+pip install fastjet==3.3.4.0rc9 energyflow 
+install eec from source; pip tries to install non-working version 0.2.0
--- main.py
+++ main.py
@@ -5,14 +5,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from types import SimpleNamespace
 from coffea.nanoevents.methods import vector
+import eec
+import fastjet
 
 #read in json config
 with open("config.json", 'r') as f:
   args = json.load(f, object_hook = lambda x : SimpleNamespace(**x))
+print("read in config")
+
+readCut = "("
+for trigger in args.triggers:
+  readCut += " (" + trigger + "==1) |"
+readCut = readCut[:-1] + ")"
+
+readCut += " & " + args.presel
+
+#print(readCut)
 
 #read in uproot file
 with uproot.open("copy.root") as f:
-  data = f['Events'].arrays(cut=args.readCut, filter_name = args.branchFilter)
+  data = f['Events'].arrays(cut=readCut, filter_name = args.branchFilter)
+
+print("read in file")
+
+#apply cuts to muons
+mask = np.ones(len(data), dtype=bool)
+for cut in args.muCuts:
+  if cut.minval == cut.maxval:
+    mask = np.logical_and(mask, ak.all(data[cut.var] == cut.minval, axis=1))
+  else:
+    if cut.minval != -999:
+      mask = np.logical_and(mask, ak.all(data[cut.var] >= cut.minval, axis=1))
+    if cut.maxval != -999:
+      mask = np.logical_and(mask, ak.all(data[cut.var] <= cut.maxval, axis=1))
+
+print("%d/%d (%0.2f%%) pass muon cuts"%(sum(mask), len(mask), 100*sum(mask)/len(mask)))
+data = data[mask]
 
 #z selection
 muons = ak.to_regular(ak.zip({
@@ -31,34 +59,41 @@ mu2 = muons[:,1]
 mass = np.abs(mu1 + mu2)
 
 massWindow = np.logical_and(mass<args.Zsel.maxMass, mass>args.Zsel.minMass)
-ID = ak.all(data[args.muons.ID], axis=1)
 charge = ak.prod(data[args.muons.charge],axis=1) == -1
 
-Zsel = np.logical_and.reduce((massWindow, ID, charge))
+Zsel = np.logical_and.reduce((massWindow, charge))
 
+print("%d/%d (%0.2f%%) pass Zsel"%(sum(Zsel), len(Zsel), 100*sum(Zsel)/len(Zsel)))
 data = data[Zsel]
 
-#jet plots
-jetPt = data[args.jets.pt]
-nJets = ak.num(jetPt)
-nConstituents = data[args.jets.nConstituents]
+#compute eecs
+jets = ak.zip({
+    "pt" : data[args.jets.pt],
+    'eta' : data[args.jets.eta],
+    'phi' : data[args.jets.phi],
+    'mass' : data[args.jets.mass]
+  },
+  behavior = vector.behavior,
+  with_name='PtEtaPhiMLorentzVector'
+)
 
-plt.hist(ak.flatten(jetPt), bins=100, density=True, histtype='step')
-plt.title("Jet pT")
-plt.xlabel("Jet pT [GeV]")
-plt.ylabel("Density")
-plt.show()
+pfcands = ak.zip({
+    "pt" : data[args.pfcands.pt],
+    'eta' : data[args.pfcands.eta],
+    'phi' : data[args.pfcands.phi],
+    'mass' : data[args.pfcands.mass],
+  },
+  behavior = vector.behavior,
+  with_name = 'PtEtaPhiMLorentzVector'
+)
 
-nJetRange = np.max(nJets) - np.min(nJets)
-plt.hist(nJets, bins=nJetRange, density=True, histtype='step')
-plt.title("nJets")
-plt.xlabel("nJets")
-plt.ylabel("Density")
-plt.show()
+#jet clustering
+jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, 0.4)
+cluster = fastjet.ClusterSequence(pfcands, jetdef)
 
-nConstituentRange = np.max(nConstituents) - np.min(nConstituents)
-plt.hist(ak.flatten(nConstituents), bins=nConstituentRange, density=True, histtype='step')
-plt.title("nConstituents")
-plt.xlabel("nConstituents")
-plt.ylabel("Density")
-plt.show()
+parts = cluster.constituents(args.minJetPt)
+
+#eec
+eec_ls = eec.EECLongestSide(2, 10, axis_range=(1e-5,1.0))
+
+#need to cast particle arrays into numpy format recognized by eec library
