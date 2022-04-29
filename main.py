0b5906b62b6ad63556c1b7d1dc4cf9b176432b88
Simon Rothman <srothman@mit.edu>
2022-04-29 15:32:14 -0500

    working proof-of-concept

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uproot
 import numpy as np
 import matplotlib.pyplot as plt
 from types import SimpleNamespace
-from coffea.nanoevents.methods import vector
+from coffea.nanoevents.methods import vector, candidate
 import eec
 import fastjet
 
@@ -20,11 +20,12 @@ readCut = readCut[:-1] + ")"
 
 readCut += " & " + args.presel
 
-#print(readCut)
+fileList = [f+":Events" for f in args.files]
+print(fileList)
 
 #read in uproot file
-with uproot.open("copy.root") as f:
-  data = f['Events'].arrays(cut=readCut, filter_name = args.branchFilter)
+#with uproot.open("copy.root") as f:
+data = uproot.concatenate(fileList, cut=readCut, filter_name = args.branchFilter)
 
 print("read in file")
 
@@ -66,34 +67,47 @@ Zsel = np.logical_and.reduce((massWindow, charge))
 print("%d/%d (%0.2f%%) pass Zsel"%(sum(Zsel), len(Zsel), 100*sum(Zsel)/len(Zsel)))
 data = data[Zsel]
 
-#compute eecs
-jets = ak.zip({
-    "pt" : data[args.jets.pt],
-    'eta' : data[args.jets.eta],
-    'phi' : data[args.jets.phi],
-    'mass' : data[args.jets.mass]
-  },
-  behavior = vector.behavior,
-  with_name='PtEtaPhiMLorentzVector'
-)
-
 pfcands = ak.zip({
     "pt" : data[args.pfcands.pt],
     'eta' : data[args.pfcands.eta],
     'phi' : data[args.pfcands.phi],
     'mass' : data[args.pfcands.mass],
+    'charge' : data[args.pfcands.charge]
   },
-  behavior = vector.behavior,
-  with_name = 'PtEtaPhiMLorentzVector'
+  behavior = candidate.behavior,
+  with_name = 'PtEtaPhiMCandidate'
 )
 
 #jet clustering
-jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, 0.4)
+jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, args.jetSize)
 cluster = fastjet.ClusterSequence(pfcands, jetdef)
 
 parts = cluster.constituents(args.minJetPt)
 
 #eec
-eec_ls = eec.EECLongestSide(2, 10, axis_range=(1e-5,1.0))
+eec_ls = eec.EECLongestSide(args.eec.N, args.eec.nBins, axis_range=(args.eec.axisMin,args.eec.axisMax))
 
-#need to cast particle arrays into numpy format recognized by eec library
+jets = ak.flatten(parts, axis=1) #remove event axis
+eecInput = ak.concatenate( (jets.pt[:,:,None], jets.eta[:,:,None], jets.phi[:,:,None], jets.charge[:,:,None]), axis=2) #stack (pt, eta, phi, charge)
+
+eec_ls(eecInput)
+midbins = eec_ls.bin_centers()
+binedges = eec_ls.bin_edges()
+binwidths = np.log(binedges[1:]) - np.log(binedges[:-1])
+hist, errs = eec_ls.get_hist_errs(0, False)
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('$\Delta R$')
+plt.ylabel("Projected %d-point correlator"%args.eec.N)
+plt.errorbar(midbins, hist/binwidths, 
+               xerr=(midbins - binedges[:-1], binedges[1:] - midbins),
+               yerr=errs/binwidths, 
+               fmt='o',lw=1.5,capsize=1.5,capthick=1,markersize=1.5)
+plt.axvline(args.jetSize, c='k')
+plt.savefig("proof.png", format='png')
+plt.show()
+
+with uproot.recreate('out.root') as f:
+  f['eec_hist'] = hist,binedges
+  f['eec_errs'] = errs,binedges
+
